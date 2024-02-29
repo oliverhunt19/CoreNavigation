7b3062082bd6796d09bd91745420e75da72c16ee
Oliver Hunt <oliver.hunt@renishaw.com>
2024-02-29 14:41:46 +0000

    base for working with collections of locations

---
+++ RoutePlanning/CollectionOfILocations.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using UnitsNet;
+
+namespace RoutePlanning
+{
+    public abstract class CollectionOfILocations<TLocation> : ReadOnlyCollection<TLocation> where TLocation : ILocation
+    {
+        private readonly PlaceSearch<TLocation> _aroundPoint;
+
+        protected CollectionOfILocations(IList<TLocation> locations) : base(locations) 
+        {
+            _aroundPoint = new PlaceSearch<TLocation>(locations.ToList());
+        }
+
+        public PlacesAroundPoint<TLocation> GetAroundPoint(LatLng centre, Length length)
+        {
+            return _aroundPoint.GetAroundPoint(centre, length);
+        }
+    }
+}
